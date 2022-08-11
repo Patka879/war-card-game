e39a07e6d3e5fd5a849bec9c161da682ecf1525a
Patrycja Mysiak <mysiak.patrycja@gmail.com>
2022-08-12 00:23:35 +0200

    styling created, function determining winning card added

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
 let deckId
+const cardsContainer = document.getElementById("cards")
+const newDeckBtn = document.getElementById("new-deck")
+const drawCardBtn = document.getElementById("draw-cards")
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
-            console.log(data)
             deckId = data.deck_id
         })
 }
 
-document.getElementById("new-deck").addEventListener("click", handleClick)
+newDeckBtn.addEventListener("click", handleClick)
 
-document.getElementById("draw-cards").addEventListener("click", () => {
+drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.cards)
-            document.getElementById('cards').innerHTML = `
-                <img src=${data.cards[0].image} />
-                <img src=${data.cards[1].image} /> `
+            cardsContainer.children[0].innerHTML = `
+                <img src=${data.cards[0].image} class="card" />
+            `
+            cardsContainer.children[1].innerHTML = `
+                <img src=${data.cards[1].image} class="card" />
+            `
         })
-        
 })
+
+function cardWinner(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"]
+    card1valueIndex = valueOptions.indexOf(card1.value)
+    card2valueIndex = valueOptions.indexOf(card2.value)
+
+    if (card1valueIndex === card2valueIndex) {
+        console.log("It's a tie") 
+    } else if (card1valueIndex > card2valueIndex) {
+        console.log("Card 1 is the winner") 
+    } else {
+        console.log("Card 2 is the winner")
+    }
+}
+
+const card1Obj = {
+    value: "7"
+}
+const card2Obj = {
+    value: "KING"
+}
+
+cardWinner(card1Obj, card2Obj)
